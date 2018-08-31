13532f38649b3d953578fe047a96b34b6a994cff
TGuikema <thom-guikema321@hotmail.com>
2018-08-31 19:56:10 +0200

    - First project commit

---
+++ ScribblePet/Assets/Scipts/MyAwesomeSecondScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyAwesomeSecondScript : MonoBehaviour {
+
+    public MyVeryFirstScript mvfs;
+
+	// Use this for initialization
+	void Start () {
+        mvfs.Hello("Thom");
+        mvfs.Interact();
+        mvfs.Showcase();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ScribblePet/Assets/Scipts/MyVeryFirstScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyVeryFirstScript : MonoBehaviour {
+
+    public void Showcase()
+    {
+        int[] intArray = new int[2] { 0, 2 };
+        int test = intArray[5];
+        Debug.LogError("THIS FUCKING MOUSE");
+    }
+
+    public void Interact()
+    {
+        Debug.LogWarning("Interacting");
+    }
+
+    public void Hello(string name)
+    {
+        Debug.Log("Hello " + name);
+    }
+}                     
