195aed7dbd4059984e59fc264776d7812c0be0ad
TGuikema <thom-guikema321@hotmail.com>
2018-10-24 23:24:39 +0200

    Adding a Toggle to the Foldout Button

---
+++ ScribblePet/Assets/Scipts/Toggle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TGuikema;
+
+public class Toggle : MonoBehaviour {
+
+    public MyButton Button;
+	// Use this for initialization
+	void Start () {
+        Button.OnButtonClick.AddListener(Toggle_Foldout);   //Subscribe the function to the inspector.
+        Toggle_Foldout();
+    }
+
+    private void OnEnable()
+    {
+
+    }
+    private void OnDisable()
+    {
+        
+    }
+
+
+    public void Toggle_Foldout()
+    {
+        if (gameObject.activeInHierarchy)
+            gameObject.SetActive(false);
+        else
+            gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ ScribblePet/Assets/toglle_PH.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TGuikema;
+
+public class toglle_PH : MonoBehaviour {
+
+    public MyButton Button;
+	// Use this for initialization
+	void Start () {
+        Button.OnButtonClick.AddListener(Toggle);
+        Toggle();
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+    private void OnDisable()
+    {
+        //Button.OnButtonClick.RemoveListener(Toggle);
+    }
+
+
+    public void Toggle()
+    {
+        if (gameObject.activeInHierarchy)
+            gameObject.SetActive(false);
+        else
+            gameObject.SetActive(true);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
