5f560d4a0684fe03bad2586d355cb04a68c074f3
TGuikema <thom-guikema321@hotmail.com>
2018-11-20 02:22:31 +0100

    Colliders and Collision Detection added.

---
+++ ScribblePet/Assets/Scipts/CheckCollision.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    bool isTriggered = false;
+
+    /*public void OnCollisionEnter2D(Collision2D other)
+    {
+        //other.gameObject.GetCom
+        Collision();
+    }*/
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Collision();
+    }
+
+    private void Collision()
+    {
+        isTriggered = true;
+        Debug.Log("Collision detected.");
+
+        OnGUI();
+    }
+
+    private void OnGUI()
+    {
+        if (isTriggered == true)
+        {
+            GUI.Label(new Rect(10, 10, 100, 20), "Collider Detected.");
+            return;
+        }
+    }
+
+
+    /* OTHER VERSION.(This does however not work completely right.)
+     * 
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Collision();
+    }
+
+    private void Collision()
+    {
+        isTriggered = true;
+        Debug.Log("Collision detected.");
+
+        Communicator();
+    }
+
+    private void Communicator()
+    {
+
+        if (isTriggered == true)
+        {
+            OnGUI();
+            return;
+        }
+
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), "Collider Detected.");
+    }
+     */
+}
